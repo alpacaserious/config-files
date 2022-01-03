22132c71a5b44d1fb1f3d2caa81d752b6bc76d22
Alpaca Serious <80459176+alpacaserious@users.noreply.github.com>
2022-01-03 10:43:37 +0000

    Update launch.sh

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,1 +1,10 @@
-#!
+#!/usr/bin/env bash
+
+# Terminate already running bars
+killall -q polybar
+
+# Wait until other bars have shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+polybar -r --config=$HOME/.config/polybar/config bar1
